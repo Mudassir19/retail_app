ccf583a40821e8a2bde9ffe4101d338922d57d5d
com <com@com-PC>
2019-09-18 18:26:29 +0530

    updated

--- RetailApplication/src/main/java/com/xebia/retail/factorypattern/DurationDiscount.java
+++ RetailApplication/src/main/java/com/xebia/retail/factorypattern/DurationDiscount.java
@@ -1,10 +1,10 @@
 package com.xebia.retail.factorypattern;
 
-public class DurationDiscount implements Discount{
+public class DurationDiscount implements Discount {
 
-	//if Duration of user>=2 years, applicable for 5% discount
+	// if Duration of user>=2 years, applicable for 5% discount
 	public int getDiscount() {
-		
+
 		System.out.println("User is applicable for the 5% Discount");
 		return 5;
 	}
